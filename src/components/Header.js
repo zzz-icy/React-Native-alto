8f1623e0ab23422186229d34877d351d1b979ffd
Han Zhang <eveline1129@163.com>
2018-10-04 17:28:08 -0500

    Added and styled Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { Text } from 'react-native'; // import deconstruring
+import { Image, View, StyleSheet } from 'react-native'; // import deconstruring
+import logo from '../../images/Alto_logo.png';
 
+const styles = StyleSheet.create({
+    viewStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgb(247, 243, 239)',
+        height: 60,
+        paddingTop: 15
+    }
+});
 const Header = () =>
     (
-        <Text>Header component</Text>
+        <View style={styles.viewStyle}>
+            <Image source={logo} />
+
+        </View>
     );
 
 export default Header;
