04ad8fe5cf93eec83f15ff484bad11e4f1db512f
Han Zhang <eveline1129@163.com>
2018-10-04 16:42:04 -0500

    added babel/runtime, Header, modified setting, js validation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/** @format */
-
-import { AppRegistry } from 'react-native';
+// import React from 'react';
+import ReactNative from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+// JS entry point to running all React Native apps
+ReactNative.AppRegistry.registerComponent('alto', () => App);
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native'; // import deconstruring
+
+const Header = () =>
+    (
+        <Text>Header component</Text>
+    );
+
+export default Header;
